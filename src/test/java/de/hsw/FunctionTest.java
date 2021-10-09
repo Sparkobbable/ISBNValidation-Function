37011cf1eee14b1ee53b6a495ab64e3b1033030c
Sophie Mümken <sophie.muemken@icloud.com>
2021-10-09 10:55:58 +0200

    Tests erweitert

--- src/test/java/de/hsw/FunctionTest.java
+++ src/test/java/de/hsw/FunctionTest.java
@@ -46,6 +46,35 @@ public class FunctionTest {
         assertEquals("valid", ret.getBody().toString(), "ISBN wird nicht validiert, Antwort lautet: "+ret.getBody().toString());
     }
 
+        /**
+     * Unit-Test für Isbn-Validierung bei invaliden Eingaben.
+     */
+    @Test
+    public void testIsbnValidatorInvalidISBN(){
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+        final Map<String, String> queryParams = new HashMap<>();
+        doReturn(queryParams).when(req).getQueryParameters();
+        
+        doAnswer(new Answer<HttpResponseMessage.Builder>() {
+            @Override
+            public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
+                HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+            }
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+        final HttpResponseMessage ret = new Function().validate(req, context);
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus(), "Falscher Returncode bei ISBN=null");
+        assertEquals("Please pass an ISBN on the query string", ret.getBody().toString(), "Fehlerhafte Antwort bei leerer ISBN, Antwort lautet: "+ret.getBody().toString());
+        queryParams.put("isbn", "jdj");
+        final HttpResponseMessage ret2 = new Function().validate(req, context);
+        assertEquals(HttpStatus.OK, ret2.getStatus());
+        assertEquals("invalid", ret2.getBody().toString(), "ISBN wird nicht validiert, Antwort lautet: "+ret.getBody().toString());
+    }
+
     /**
      * Unit-Test für korrekte Prüfzifferberechnung.
      */
@@ -74,6 +103,39 @@ public class FunctionTest {
     }
 
     /**
+     * Unit-Test für korrekte Prüfzifferberechnung.
+     */
+    @Test
+    public void testCheckDigitCalculationInvalidIsbn(){
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+        final Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("isbn", "3928");
+        doReturn(queryParams).when(req).getQueryParameters();
+        
+        doAnswer(new Answer<HttpResponseMessage.Builder>() {
+            @Override
+            public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
+                HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+            }
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+        final HttpResponseMessage ret = new Function().calculate(req, context);
+        
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus());
+        assertEquals("Fehler: Der ISBN-String hat die falsche Länge!", ret.getBody().toString(), "Fehler wurde falsch ausgegeben, Antwort: "+ret.getBody().toString());
+
+        queryParams.put("isbn", "123456h89");
+        final HttpResponseMessage ret2 = new Function().calculate(req, context);
+        
+        assertEquals(HttpStatus.BAD_REQUEST, ret2.getStatus());
+        assertEquals("Fehler: Der ISBN-String enthält unerwartete Zeichen!", ret2.getBody().toString(), "Fehler wurde falsch ausgegeben, Antwort: "+ret.getBody().toString());
+    }
+
+    /**
      * Unit-Test für korrekte ISBN-Erzeugung.
      */
     @Test
@@ -93,12 +155,50 @@ public class FunctionTest {
                 return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
             }
         }).when(req).createResponseBuilder(any(HttpStatus.class));
-        
+
         final ExecutionContext context = mock(ExecutionContext.class);
         doReturn(Logger.getGlobal()).when(context).getLogger();
         final HttpResponseMessage ret = new Function().build(req, context);
         
         assertEquals(HttpStatus.OK, ret.getStatus());
         assertEquals("3-9284-7532-0", ret.getBody().toString(), "ISBN wurde falsch berechnet, Antwort: "+ret.getBody().toString());
+    }
+
+        /**
+     * Unit-Test für ISBN-Erzeugung mit fehlerhaften Parametern.
+     */
+    @Test
+    public void testeIsbnErzeugungInvalidParams(){
+        @SuppressWarnings("unchecked")
+        final HttpRequestMessage<Optional<String>> req = mock(HttpRequestMessage.class);
+        final Map<String, String> queryParams = new HashMap<>();
+        queryParams.put("gnumber", "3");
+        queryParams.put("vnumber", "9284");
+        doReturn(queryParams).when(req).getQueryParameters();
+        
+        doAnswer(new Answer<HttpResponseMessage.Builder>() {
+            @Override
+            public HttpResponseMessage.Builder answer(InvocationOnMock invocation) {
+                HttpStatus status = (HttpStatus) invocation.getArguments()[0];
+                return new HttpResponseMessageMock.HttpResponseMessageBuilderMock().status(status);
+            }
+        }).when(req).createResponseBuilder(any(HttpStatus.class));
+
+        final ExecutionContext context = mock(ExecutionContext.class);
+        doReturn(Logger.getGlobal()).when(context).getLogger();
+        final HttpResponseMessage ret = new Function().build(req, context);
+        
+        assertEquals(HttpStatus.BAD_REQUEST, ret.getStatus());
+        assertEquals("Please pass a gnumber(Gruppennummer), vnumber(Verlagsnummer) and tnumber(Titelnummer) on the query string", ret.getBody().toString(), "Falscher Fehler bei ISBN-Erzeugung, Antwort: "+ret.getBody().toString());
+        
+        queryParams.put("tnumber", "75324");
+        final HttpResponseMessage ret2 = new Function().build(req, context);
+        assertEquals(HttpStatus.BAD_REQUEST, ret2.getStatus());
+        assertEquals("Fehler: Der ISBN-String hat die falsche Länge!", ret2.getBody().toString(), "Falscher Fehler bei ISBN-Erzeugung, Antwort: "+ret2.getBody().toString());
+        
+        queryParams.put("tnumber", "753t");
+        final HttpResponseMessage ret3 = new Function().build(req, context);
+        assertEquals(HttpStatus.BAD_REQUEST, ret3.getStatus());
+        assertEquals("Fehler: Der ISBN-String enthält unerwartete Zeichen!", ret3.getBody().toString(), "Falscher Fehler bei ISBN-Erzeugung, Antwort: "+ret3.getBody().toString());
     }
 }
